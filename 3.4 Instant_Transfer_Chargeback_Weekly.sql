d8902d7a29fc351e74597146f789276f903f288b
jingchime <78095842+jingchime@users.noreply.github.com>
2021-03-08 15:13:21 -0500

    Update 3.4 Instant_Transfer_Chargeback_Weekly.sql

--- 3.4 Instant_Transfer_Chargeback_Weekly.sql
+++ 3.4 Instant_Transfer_Chargeback_Weekly.sql
@@ -22,8 +22,7 @@ WITH instant_transfers AS (select id, user_id,transaction_code, transaction_time
 SELECT DATE_TRUNC('week', TRANSACTION_TIMESTAMP)::DATE AS ORIGINAL_TXN_week,	
     COUNT(DISTINCT CASE WHEN transaction_code in ('PMDB', 'PMTP') THEN instant_transfers.user_id ELSE NULL END) AS "instant_transfers.chargeback_users",	
     COALESCE(SUM(CASE WHEN transaction_code in ('PMDB', 'PMTP') THEN instant_transfers.transaction_amount ELSE NULL END  ), 0) AS "instant_transfers.instant_transfers_transaction_volume"	
-FROM CHIME.FINANCE.MEMBERS  AS members	
-LEFT JOIN instant_transfers ON (members."ID") = instant_transfers.user_id	
+FROM instant_transfers	
 WHERE instant_transfers.transaction_timestamp  >= TO_TIMESTAMP('2020-11-1')	
        and instant_transfers.user_id in (select distinct user_id	
           from ANALYTICS.LOOKER."TRANSACTIONS" transactions	
